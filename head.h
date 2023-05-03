4de55f805388308683e5bb53d64c20935144d4dc
HumanMan12138 <124561354+HumanMan12138@users.noreply.github.com>
2023-05-03 10:29:01 +0800

    Add files via upload

---
+++ head.h
@@ -0,0 +1,13 @@
+#ifndef DUNGEON_H
+#define DUNGEON_H
+
+
+int battleplane();
+int Martingale(int capital);
+int Casino(int money);
+int riddles(int coins);
+int dAlembert(int currentBalance);
+
+
+#endif
+
